e1879032b0206e84ccb14e71dd0e4402be5d6ccc
Hezakai <graves.eric@gmail.com>
2023-08-11 13:28:31 -0500

    dependa's and app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const hbars = require('express-handlebars');
+const bp = require('body-parser');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('INDEX TEST TEXT'))
+
+const PORT = process.env.DB_PORT
+
+app.listen(PORT, console.log(`Server started on port: ${PORT}`));
