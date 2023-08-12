bc4e2ad997b1a481be672e16c168759461430614
Hezakai <graves.eric@gmail.com>
2023-08-12 07:58:29 -0500

    fixed route

--- routes/inventory.js
+++ routes/inventory.js
@@ -3,12 +3,18 @@ const router = express.Router();
 const dv = require('../config/dbConn');
 const Inv = require('../models/Inventory')
 
-router.get('/', (req, res) => 
-Inv.findAll()
-    .then(inv => {
-        console.log(inv);
-        res.sendStatus(200);
-    })
-    .catch(err => console.log(err)));
+router.get('/', (req, res) => {
+    Inv.findAll().then((invData) => {
+        res.json(invData);
+    });
+});
+
+
+// Inv.findAll()
+//     .then(inv => {
+//         console.log(inv);
+//         res.sendStatus(200);
+//     })
+//     .catch(err => console.log(err)));
 
 module.exports = router;
