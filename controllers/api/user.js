7537cc1342947424c6983b7260bd484e2799aa12
Hezakai <graves.eric@gmail.com>
2023-08-15 08:18:21 -0500

    fixed user route

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const { User } = require('../../models/')
 
-router.get('/'), (req, res) => {
+router.get('/', (req, res) => {
     User.findAll().then((invData) => {
         res.json(invData);
     });
-}
+});
 
 router.post('/', async (req, res) => {
     try {
@@ -18,8 +18,89 @@ router.post('/', async (req, res) => {
             res.status(200).json(userData)
         });
     } catch (err) {
-        res.status(400).json(err);
+        console.error(err);
+        res.status(400).json({ error: err.message });
     }
 });
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        if (!userData) {
+            res.status(404).json({ message: 'User not found'});
+            return;
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!userData) {
+            res.status(404).json({ message: 'User not found'});
+            return;
+        }
+
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+        
+        res.json({ user: userData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+  
+  router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
 
 module.exports = router;
