512c8925170d5744329a1b688bb04479651204aa
Hezakai <graves.eric@gmail.com>
2023-08-16 06:56:53 -0500

    added partials for inv and recipes

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.APP_PORT;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-
 const sess = {
   secret: 'Super secret secret',
   cookie: {
@@ -28,6 +25,13 @@ const sess = {
     db: sequelize
   })
 };
+
+// Set up Handlebars.js engine with custom helpers and partials
+const hbs = exphbs.create({
+  helpers,
+  defaultLayout: 'main',
+  partialsDir: path.join(__dirname, 'views/partials/')
+});
 
 app.use(session(sess));
 
