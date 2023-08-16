64b000729cd3502a3a81e9d902548f56bec5a448
Hezakai <graves.eric@gmail.com>
2023-08-15 20:15:17 -0500

    fixed handle bars and got auth working

--- server.js
+++ server.js
@@ -4,18 +4,15 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
-const bp = require('body-parser');
-// const db = require('./config/dbConn');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.APP_PORT;
 
-
 // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -35,16 +32,16 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('handlebars', exphbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(bp.json());
-// app.use(bp.urlencoded({ extended: false }));
 
 app.use(routes);
+
+app.get('/test', (req, res) => res.send('Test Route'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
