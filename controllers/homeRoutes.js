fcbb70bb95f88afe6331e6cc32bf32b21bd0ac6e
Hezakai <graves.eric@gmail.com>
2023-08-18 11:30:26 -0500

    fixed image route and added rec seeds

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,61 +1,63 @@
-const router = require('express').Router();
-const { User, Inv, Rec, Ing, Step } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Inv, Rec, Ing, Step } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-    try {
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        logged_in: req.session.logged_in 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ['password'] },
-        include: [  // <-- Update here to fetch associated data
-            {
-                model: Inv
-            },
-            {
-                model: Rec,
-                include: [
-                    { model: Ing },
-                    { model: Step }
-                ]
-            }
-        ]
-      });
-  
-      const user = userData.get({ plain: true });
-      console.log(user);
-  
-      res.render('profile', {
-        user,
-        logged_in: true
-      });
-    } catch (err) {
-      console.error("Error loading profile: ", err);
-      res.status(500).json(err);
+router.get("/", async (req, res) => {
+  try {
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  
 });
 
-router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }
-  
-    res.render('login');
-  });
-  
+// Use withAuth middleware to prevent access to route
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: Inv,
+        },
+        {
+          model: Rec,
+          include: [
+            { model: Ing },
+            { model: Step },
+          ],
+        },
+      ],
+      order: [
+        [Rec, Step, 'number', 'ASC']
+      ],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log(user);
+
+    res.render("profile", {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    console.error("Error loading profile: ", err);
+    res.status(500).json(err);
+  }
+});
+
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -7,34 +7,96 @@ const stepData = require('./stepData.json');
 const userData = require('./userData.json');
 
 const seedDB = async () => {
-    await sequelize.sync({force: true});
+    await sequelize.sync({ force: true });
 
-    const user = await User.bulkCreate(userData, {  
-        individualHooks: true,
-        returning: true,
-    });
+    const users = [];
+    for (let data of userData) {
+        const user = await User.create(data, {
+            individualHooks: true,
+            returning: true
+        });
+        users.push(user);
+    }
 
-    const inv = await Inv.bulkCreate(invData, {  
-        individualHooks: true,
-        returning: true,
-    });
+    const invs = [];
+    for (let data of invData) {
+        const inv = await Inv.create(data, {
+            individualHooks: true,
+            returning: true
+        });
+        invs.push(inv);
+    }
 
-    const rec = await Rec.bulkCreate(recData, {  
-        individualHooks: true,
-        returning: true,
-    });
+    const recs = [];
+    for (let data of recData) {
+        const rec = await Rec.create(data, {
+            individualHooks: true,
+            returning: true
+        });
+        recs.push(rec);
+    }
 
-    const ing = await Ing.bulkCreate(ingData, {  
-        individualHooks: true,
-        returning: true,
-    });
+    const ings = [];
+    for (let data of ingData) {
+        const ing = await Ing.create(data, {
+            individualHooks: true,
+            returning: true
+        });
+        ings.push(ing);
+    }
 
-    const step = await Step.bulkCreate(stepData, {  
-        individualHooks: true,
-        returning: true,
-    });
+    const steps = [];
+    for (let data of stepData) {
+        const step = await Step.create(data, {
+            individualHooks: true,
+            returning: true
+        });
+        steps.push(step);
+    }
 
     process.exit(0);
 };
 
 seedDB();
+
+
+// const sequelize = require('../config/connection');
+// const { User, Inv, Rec, Ing, Step, List } = require('../models');
+// const ingData = require('./ingData.json');
+// const invData = require('./invData.json');
+// const recData = require('./recData.json');
+// const stepData = require('./stepData.json');
+// const userData = require('./userData.json');
+
+// const seedDB = async () => {
+//     await sequelize.sync({force: true});
+
+//     const user = await User.bulkCreate(userData, {  
+//         individualHooks: true,
+//         returning: true,
+//     });
+
+//     const inv = await Inv.bulkCreate(invData, {  
+//         individualHooks: true,
+//         returning: true,
+//     });
+
+//     const rec = await Rec.bulkCreate(recData, {  
+//         individualHooks: true,
+//         returning: true,
+//     });
+
+//     const ing = await Ing.bulkCreate(ingData, {  
+//         individualHooks: true,
+//         returning: true,
+//     });
+
+//     const step = await Step.bulkCreate(stepData, {  
+//         individualHooks: true,
+//         returning: true,
+//     });
+
+//     process.exit(0);
+// };
+
+// seedDB();
