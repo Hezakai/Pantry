01616f3988d689ed905db1cc0638d7d6083ee68a
Hezakai <graves.eric@gmail.com>
2023-08-19 07:21:58 -0500

    recipe handlebar and route setup.  Removed style

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,6 +49,40 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+router.get("/recipe/:id", withAuth, async (req, res) => {
+  try {
+    const recipeId = req.params.id;
+
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [
+        {
+          model: Rec,
+          where: { id: recipeId }, 
+          include: [
+            { model: Ing },
+            { model: Step },
+          ],
+        },
+      ],
+      order: [
+        [Rec, Step, 'number', 'ASC']
+      ],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log(user);
+
+    res.render("recipe", {
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    console.error("Error loading recipe: ", err);
+    res.status(500).json(err);
+  }
+});
+
 
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
